edfdcf49b31c71b5e5e23b5b1ef8a8227e5f9a71
lbeckman314 <lbeckman314@gmail.com>
2019-07-08 21:59:43 -0700

    Update Jenkinsfile.

--- .build.sh
+++ .build.sh
@@ -7,18 +7,16 @@ demo=/var/www/demo/programs
 src=/var/www/pkgs/$project/src
 obj=matriz.sh
 
-pwd
 git pull origin master
 
-echo "move compressed files and sha/gpg signatures to packages directory"
+echo "compress"
 git archive --format=tar -o $project.tar.gz HEAD
 git archive --format=zip -o $project.zip HEAD
 
+echo "sign"
 sha256sum *.tar.gz *.zip > sha256sums.txt
 gpg --pinentry-mode loopback --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
 
+echo "copy"
 mv $project.tar.gz $project.zip sha256sums.txt* $pkgs
-
-cd $src
-git pull
 cp $obj $demo
